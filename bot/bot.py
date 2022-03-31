6d4141207885e522e8a12e487f09c690e3769447
DaegyeomHa <combbm@naver.com>
2022-03-31 19:42:13 +0900

    feat: check account log using database

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from sqlite3 import Connection
 
 import discord
 from discord.ext import commands, tasks
@@ -7,23 +8,34 @@ from common import logger
 
 
 class Bot(commands.Bot):
-    def __init__(self, bank, channel_id, **options):
+    def __init__(self, connection: Connection, bank, channel_id, **options):
         super().__init__(**options)
+        self.connection = connection
         self.bank = bank
         self.channel_id = channel_id
 
     async def on_ready(self):
-        logger.info('Start discord bot successfully.')
+        logger.info("Start discord bot successfully.")
 
-        self.sendBankAlert.start()
-        logger.info('Register schedule job for bank alert.')
+        self.checkAccountLog.start()
+        logger.info("Register schedule job for bank alert.")
 
     @tasks.loop(hours=4)
-    async def sendBankAlert(self):
+    async def checkAccountLog(self):
         channel = discord.utils.get(self.get_all_channels(), id=int(self.channel_id))
         for i in self.bank.getData():
-            embed = discord.Embed(title=f"입금 확인 ({i.who})",
-                                  description=f"넷플릭스 {i.cost}원 입금 확인",
-                                  color=0xF93A2F,
-                                  timestamp=datetime.strptime(f"{i.date} {i.time}", "%Y-%m-%d %H:%M:%S"))
-            await channel.send(embed=embed)
+            cur = self.connection.cursor()
+            cur.execute(f"SELECT * FROM data WHERE date='{i.date}' and name='{i.who}';")
+
+            if cur.fetchone() is None:
+                logger.info("Found new account log.")
+
+                embed = discord.Embed(title=f"입금 확인 ({i.who})",
+                                      description=f"넷플릭스 {i.cost}원 입금 확인",
+                                      color=0xF93A2F,
+                                      timestamp=datetime.strptime(f"{i.date} {i.time}", "%Y-%m-%d %H:%M:%S"))
+                await channel.send(embed=embed)
+
+                insert_cur = self.connection.cursor()
+                insert_cur.execute(f"INSERT INTO data VALUES ('{i.date}', '{i.who}');")
+                self.connection.commit()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from bank.bank import Bank
 from bot import BotCommands
 from bot import bot
@@ -10,9 +12,13 @@ def run():
     bank = Bank(config.bank_id, config.bank_password, config.account_password, config.bank_cost)
     bank.fetchData()
 
-    client = bot.Bot(bank, config.discord_channel, command_prefix="!")
+    connection = sqlite3.connect('data.db')
+
+    client = bot.Bot(connection, bank, config.discord_channel, command_prefix="!")
     client.add_cog(BotCommands(client))
     client.run(config.discord_token)
+
+    connection.close()
 
 
 if __name__ == '__main__':
