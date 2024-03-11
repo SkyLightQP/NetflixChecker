cb43b7cab724d0cb8e3027816de14184a191be21
Daegyeom Ha <combbm@gmail.com>
2024-03-11 14:58:31 +0900

    feat: initialize spring boot

---
+++ src/main/kotlin/me/daegyeo/netflixchecker/NetflixcheckerApplication.kt
@@ -0,0 +1,11 @@
+package me.daegyeo.netflixchecker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NetflixcheckerApplication
+
+fun main(args: Array<String>) {
+    runApplication<NetflixcheckerApplication>(*args)
+}
---
+++ src/test/kotlin/me/daegyeo/netflixchecker/NetflixcheckerApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.daegyeo.netflixchecker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NetflixcheckerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
