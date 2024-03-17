c0e291ed213de02594c4bee67c00de9bdb1db4d6
Daegyeom Ha <combbm@gmail.com>
2024-03-17 18:52:39 +0900

    feat: implement BankCrawler using selenium

---
+++ src/main/kotlin/me/daegyeo/netflixchecker/data/AccountData.kt
@@ -0,0 +1,9 @@
+package me.daegyeo.netflixchecker.data
+
+data class AccountData(
+    val time: String,
+    val date: String,
+    val cost: String,
+    val who: String,
+    val costMonth: String
+)
